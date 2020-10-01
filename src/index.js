2200649b64631fffb56ffc875fa303b168486bd6
Sergei Norvaishas <snorvaishas@yandex.ru>
2020-10-01 20:13:19 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    if (Array.isArray(array) === false || array.length === 0) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            let wasSwap = false;
+
+            for (let j = i; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    const swap = array[i];
+                    array[i] = array[j];
+                    array[j] = swap;
+                    wasSwap = true;
+                }
+            }
+
+            if (!wasSwap) break;
+        }
+        return array[0];
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+    if (Array.isArray(array) === false || array.length === 0) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            let wasSwap = false;
+
+            for (let j = i; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    const swap = array[i];
+                    array[i] = array[j];
+                    array[j] = swap;
+                    wasSwap = true;
+                }
+            }
+
+            if (!wasSwap) break;
+        }
+        return array[0];
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    if (Array.isArray(array) === false || array.length === 0) {
+        return 0;
+    } else {
+        let accum = 0;
+
+        for (let i = 0; i < array.length; i++) {
+            accum += array[i];
+        }
+        return accum / array.length;
+    }
 }
